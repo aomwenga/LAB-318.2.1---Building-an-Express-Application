199c39865e40dc0fd2ab7ebf967bc6d7af6105aa
AOmwenga <aomwenga@gmail.com>
2024-10-10 12:55:47 -0500

    server running

---
+++ server.mjs
@@ -0,0 +1,15 @@
+// server.mjs
+import express from "express";
+
+// instance of express
+const app = express();
+const PORT = 3000;
+
+// Middleware
+
+// Routes
+
+// initialize
+app.listen(PORT, () => {
+  console.log(`Server is running on Port: ${PORT}`);
+});
